465120052e97f6b5d4e6ced91ac6fe5d74910149
reggi <thomas@reggi.com>
2014-02-02 01:55:30 -0500

    backstretch

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 $(document).ready(function() {
+    $(".background").backstretch('./both.jpg');
+    $(".head").backstretch("./head.png");
     $(".head").draggable({
         drag: function(event, ui) {
             var position = $(".ui-draggable-dragging").position();
-            pointer = $('<img>').attr({
-                'src': './ricks-head.png'
-            });
+            pointer = $('<div>').addClass("head-new").backstretch("./head.png");
             $(document.body).append(pointer);
             pointer.css({
                 "position": 'absolute',
                 "top": position.top,
                 "left": position.left
-            })
+            });
         }
     });
 });
